3646ef3e47fcd624bdd32d2f55b505c7d7927c83
Greg Yang <gregdyang@gmail.com>
2013-11-06 23:16:18 -0500

    Initial commit

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'cde0836970baca98a75ed5a96d1b19cbd46b5855c14605111cc03cc99a01bf4b4b98549b51cd4300a4dae72775ffd45f89d4d835392255b558a39042cb0b39e3'
---
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '05203296469b778fcb7c059b77b62de641159be8c3eb36d71db9f512d092b6347cd2f500ee2f850721a01c88d8c16e8b211070b9f9f0dbbc382de5423ec05a53'
